7b40abee81f251c131c80fc32df5a042d493adb7
Marcello Sautto <marcellosautto@gmail.com>
2021-12-10 16:23:06 -0800

    added login screen UI

---
+++ NotEBookDesktop/NotEBookDesktop/Login.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace NotEBookDesktop
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void updateUsernameInput(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void updatePasswordInput(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
