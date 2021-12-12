20a6ef0e812f7d85f402d1091afd28fab252d3a9
Marcello Sautto <marcellosautto@gmail.com>
2021-12-12 01:17:12 -0800

    Admin can login with credentials stored in local SQL DB

--- NotEBookDesktop/NotEBookDesktop/Login.xaml.cs
+++ NotEBookDesktop/NotEBookDesktop/Login.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,7 +38,40 @@ namespace NotEBookDesktop
 
         private void LoginBtn_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NotEBookUserAuthDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 
+            try
+            {
+                if (sqlCon.State == ConnectionState.Closed)
+                    sqlCon.Open();
+
+                String query = "SELECT COUNT(1) FROM [Table] WHERE Username=@Username AND Password=@Password";
+
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Username", UsernameInput.Text);
+                sqlCmd.Parameters.AddWithValue("@Password", PasswordInput.Password);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if(count == 1)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Username or Password is Incorrect");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
         }
     }
 }
