fac20a6e98774fd5458a0acf5049663616fffb71
HectorOrtiz618 <90296733+HectorOrtiz618@users.noreply.github.com>
2022-02-06 17:54:41 -0800

    Added Calendar to Desktop
    -Added Calendar functionality, currently appears as new popup window
    -Added ability to add events to selected dates
    
    TODO
    -Remove Button causes a crash to desktop when not in debug mode
    -Fix styling to better reflect that of Main Window
    -Events aren't saved, waiting until we add database functionality to desktop

---
+++ NotEBookDesktop/NotEBookDesktop/Calendar.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotEBookDesktop
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Calendar : Window
+    {
+        public Calendar()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterEvent_Click(object sender, RoutedEventArgs e)
+        {
+            //Check if EventDate and EventTextBox is empty, if so throw an error to user
+            if(EventDate.SelectedDate == null)
+            {
+                MessageBox.Show("No date has been picked", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if(String.IsNullOrEmpty(EventTextBox.Text))
+            {
+                MessageBox.Show("No Event added in Textbox", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            //If both have entrys then push to EventView and clear entrys
+            else
+            {
+                //add events
+                EventItem E = new EventItem();
+                E.Date = EventDate.SelectedDate.Value.ToString("MM/dd/yyyy");
+                E.Event = EventTextBox.Text.ToString();
+
+                //push event to listview
+                EventList.Items.Add(E);
+                //clear entrys form EventDate and EventTextBox
+                EventDate.SelectedDate = null;
+                EventTextBox.Text = String.Empty;
+            }
+        }
+
+        private void RemoveEvent_Click(object sender, RoutedEventArgs e)
+        {
+            //See which Index is slecected to be removed
+            if(EventList.SelectedIndex == -1)
+            {
+                //do nothing
+            }
+            else
+            {
+                //EventList.Items.Remove(EventList.SelectedIndex); DOSEN'T WORK
+
+
+
+                //remove selected index THIS CAUSES A CRASH TO DESKTOP
+                //foreach (ListViewItem eachItem in EventList.SelectedItems)
+                //{
+                //    EventList.Items.Remove(eachItem);
+                //}
+            }
+        }
+    }
+}
--- NotEBookDesktop/NotEBookDesktop/MainWindow.xaml.cs
+++ NotEBookDesktop/NotEBookDesktop/MainWindow.xaml.cs
@@ -112,6 +112,20 @@ namespace NotEBookDesktop
                 fs.Close();
             }
         }
+        private void Calendar_Click(object sender, RoutedEventArgs e)
+        {
+            //Open Calendar page as new window
+
+            Calendar cal = new Calendar();
+            cal.Show();
+            //string messageBoxText = "NO Calendar fo you";
+            //string caption = "Buy one!";
+            //MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            //MessageBoxImage icon = MessageBoxImage.Warning;
+            //MessageBoxResult result;
+
+            //result = MessageBox.Show(messageBoxText, caption, button, icon, MessageBoxResult.Yes);
+        }
     }
  
 }
