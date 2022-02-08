f23a03c31c8bddfc34c5d85fc530f795df0efcb6
Marcello Sautto <marcellosautto@gmail.com>
2022-02-07 20:32:44 -0800

    Merged Calendar -> Desktop Branch

--- NotEBookDesktop/NotEBookDesktop/obj/Debug/net5.0-windows/Calendar.g.i.cs
+++ NotEBookDesktop/NotEBookDesktop/obj/Debug/net5.0-windows/Calendar.g.i.cs
@@ -95,13 +95,13 @@ namespace NotEBookDesktop {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/NotEBookDesktop;component/calendar.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/NotEBookDesktop;V1.0.0.0;component/calendar.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Calendar.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -111,7 +111,7 @@ namespace NotEBookDesktop {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.9.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
