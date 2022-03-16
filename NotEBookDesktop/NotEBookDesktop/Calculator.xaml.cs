55b391e8a86d4c19b92ec8d507b64e655496b49d
Isaiah <isaacgalvan600@gmail.com>
2022-03-16 13:37:55 -0700

    Commit 3
    Fixed errors in mainWindow.xml Calculator button
    Updated Calculator.xaml.cs
    Updated mainWindow.cs
    
    Project compiled successfully.

--- NotEBookDesktop/NotEBookDesktop/Calculator.xaml.cs
+++ NotEBookDesktop/NotEBookDesktop/Calculator.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace NotEBookDesktop
 {
@@ -256,7 +257,7 @@ namespace NotEBookDesktop
             button.Content = angleUnitSymbols[angleUnit];
         }
 
-=
+
         /// Changes the trigonometric functions mode to Normal, Hyperbolic or Arc
         private void trig_mode_button_Click(object sender, RoutedEventArgs e)
         {
