1c3bebda959fe146a32815c7cb649171d9370dc2
HectorOrtiz618 <90296733+HectorOrtiz618@users.noreply.github.com>
2022-03-28 09:04:44 -0700

    Added Updated Components
    Added Updated Components fro Calendar

---
+++ NotEBookDesktop/NotEBookDesktop/Angles.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotEBookDesktop
+{
+    class Angles
+    {
+        public enum units
+        {
+            RADIANS,  // Default unit
+            DEGREES
+        }
+
+        public class Converter
+        {
+            /// Converts the given angle to degrees
+            
+            public static double degrees(double angle, units angleUnit)
+            {
+                if (angleUnit == units.RADIANS)
+                    return angle * 180 / Math.PI;
+
+                else if (angleUnit == units.DEGREES)
+                    return angle;
+                else
+                {
+                    Exception error = new Exception("Invalid parameters");
+                    throw error;
+                }
+            }
+
+            /// Converts the given angle to radians
+            public static double radians(double angle, units angleUnit)
+            {
+                // Avoid extra computation
+                if (angleUnit == units.RADIANS)
+                    return angle;
+
+                return degrees(angle, angleUnit) * Math.PI / 180;
+            }
+
+
+
+        }
+    }
+}
--- NotEBookDesktop/NotEBookDesktop/Calendar.xaml.cs
+++ NotEBookDesktop/NotEBookDesktop/Calendar.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Runtime.InteropServices;
-using Path = System.IO.Path;
+//using Path = System.IO.Path;
+using System.ComponentModel;
 
 namespace NotEBookDesktop
 {
@@ -27,18 +29,37 @@ namespace NotEBookDesktop
         public Calendar()
         {
             InitializeComponent();
-        }
+            //lvwColumnSorter = new ListViewColumnSorter();
 
+            //Sorting Items in EventView
+            CollectionView EventView = (CollectionView)CollectionViewSource.GetDefaultView(EventList.Items);
+            EventView.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Ascending));
+            EventView.Refresh();
+            //view.Filter = DateFilter;
+
+
+            //Calendar Date Filtering
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(EventList.Items);
+            view.Filter = DateFilter;
+            //EventList.Items.SortDescriptions.Add(new SortDescription("Date", ListSortDirection.Ascending));
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
         private void EnterEvent_Click(object sender, RoutedEventArgs e)
         {
             //Check if EventDate and EventTextBox is empty, if so throw an error to user
             if (EventDate.SelectedDate == null)
             {
-                MessageBox.Show("No date has been picked", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("No date has been picked", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             else if (String.IsNullOrEmpty(EventTextBox.Text))
             {
-                MessageBox.Show("No Event added in Textbox", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("No Event added in Textbox", "Alert", MessageBoxButton.OK, MessageBoxImage.Error);
             }
             //If both have entrys then push to EventView and clear entrys
             else
@@ -54,6 +75,8 @@ namespace NotEBookDesktop
                 EventDate.SelectedDate = null;
                 EventTextBox.Text = String.Empty;
             }
+            CollectionView EventView = (CollectionView)CollectionViewSource.GetDefaultView(EventList.Items);
+            EventView.Refresh();
         }
 
         private void RemoveEvent_Click(object sender, RoutedEventArgs e)
@@ -69,6 +92,8 @@ namespace NotEBookDesktop
             {
                 EventList.Items.Remove(eachItem);
             }
+            CollectionView EventView = (CollectionView)CollectionViewSource.GetDefaultView(EventList.Items);
+            EventView.Refresh();
         }
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
@@ -79,16 +104,21 @@ namespace NotEBookDesktop
             //using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "Events")))
 
             if (EventList.Items.Count > 0)// Only save if we have a collection to save
-            { 
-                using (StreamWriter outputFile = new StreamWriter("Events.ev"))
+            {
+                SaveFileDialog sf = new SaveFileDialog();
+                sf.Filter= "NotEBook Events (*.ev)|*.ev";
+                if (sf.ShowDialog() == true)
                 {
-                    foreach (EventItem eachItem in EventList.Items)
+                    using (StreamWriter outputFile = new StreamWriter(sf.FileName))
                     {
-                        //write to file
-                        outputFile.WriteLine(eachItem.Date + "\t" + eachItem.Event);
+                        foreach (EventItem eachItem in EventList.Items)
+                        {
+                            //write to file
+                            outputFile.WriteLine(eachItem.Date + "\t" + eachItem.Event);
+                        }
                     }
+                    MessageBox.Show("File Saved Successfully", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
                 }
-                MessageBox.Show("File Saved Successfully", "Alert", MessageBoxButton.OK, MessageBoxImage.Information);
             }
         }
 
@@ -104,43 +134,51 @@ namespace NotEBookDesktop
             
             //Clear EventList
             EventList.Items.Clear();
-            try
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "NotEBook Events(*.ev)| *.ev";
+            if (od.ShowDialog() == true)
             {
-                //using (var sr = new StreamReader(Path.Combine(docPath, "Events")))
-                using (var sr = new StreamReader("Events.ev"))
+                try
                 {
-                    while (sr.Peek() >= 0)// 0 is the EOF
+                    //using (var sr = new StreamReader(Path.Combine(docPath, "Events")))
+                    using (var sr = new StreamReader(od.FileName))
                     {
-                        c = (char)sr.Read();
-                        //Read the stream, and write the string to EventList, first read until \t is found to push to Date Colounm, then read til \n to push to Event Colounm, repeat until EOF
-                        if (c != '\t' && isDate)// Read date until we hit a \t char
+                        while (sr.Peek() >= 0)// 0 is the EOF
                         {
-                            Date.Append(c);
+                            c = (char)sr.Read();
+                            //Read the stream, and write the string to EventList, first read until \t is found to push to Date Colounm, then read til \n to push to Event Colounm, repeat until EOF
+                            if (c != '\t' && isDate)// Read date until we hit a \t char
+                            {
+                                Date.Append(c);
+                            }
+                            if (c == '\t' && isDate)// Set isDate to false and continue to Event
+                            {
+                                isDate = false;
+                            }
+                            if (c != '\n' && c != '\r' && !isDate)// Read Event until we hit \n char, the \r char ruins the formatting and adds unwanted whitespace, so we have to remove them from the string
+                            {
+                                Event.Append(c);
+                            }
+                            if (c == '\n' && !isDate)//Reset everything to prepare for the next line
+                            {
+                                EventItem E = new EventItem();
+                                E.Date = Date.ToString();
+                                E.Event = Event.ToString();
+                                EventList.Items.Add(E);
+                                isDate = true;
+                                Date.Clear();
+                                Event.Clear();
+
+                            }
                         }
-                        if (c == '\t' && isDate)// Set isDate to false and continue to Event
-                        {
-                            isDate = false;
-                        }
-                        if (c != '\n' && c != '\r' && !isDate)// Read Event until we hit \n char, the \r char ruins the formatting and adds unwanted whitespace, so we have to remove them from the string
-                        {
-                            Event.Append(c);
-                        }
-                        if (c == '\n' && !isDate)//Reset everything to prepare for the next line
-                        {
-                            EventItem E = new EventItem();
-                            E.Date = Date.ToString();
-                            E.Event = Event.ToString();
-                            EventList.Items.Add(E);
-                            isDate = true;
-                            Date.Clear();
-                            Event.Clear();
-                        }
+                        CollectionView EventView = (CollectionView)CollectionViewSource.GetDefaultView(EventList.Items);
+                        EventView.Refresh();
                     }
                 }
-            }
-            catch (IOException ex)
-            {
-                MessageBox.Show("Unable to load file: " + ex.Message, "Couldn't Find File", MessageBoxButton.OK, MessageBoxImage.Information);
+                catch (IOException ex)
+                {
+                    MessageBox.Show("Unable to load file: " + ex.Message, "Couldn't Find File", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
             }
 
         }
@@ -149,7 +187,22 @@ namespace NotEBookDesktop
         {
             //Filters events that have the selected dates
             //LeftCalendar.SelectedDates.Add();
+            CollectionViewSource.GetDefaultView(EventList.Items).Refresh();
+        }
 
+        private void Calendar_Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private bool DateFilter(object item)
+        {
+            if (LeftCalendar.SelectedDate == null) // no items selected
+            {
+                return true;
+            }
+            else
+                return (item as EventItem).Date.Equals(LeftCalendar.SelectedDate.Value.ToString("MM/dd/yyyy"));
+            
         }
     }
 }
