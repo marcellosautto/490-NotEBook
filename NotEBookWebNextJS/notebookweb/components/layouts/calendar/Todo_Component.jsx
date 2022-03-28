fd908e9567f7033b1b1075c46d31ee79b6dc95d4
Marcello Sautto <marcellosautto@gmail.com>
2022-03-28 10:31:57 -0700

    updated formatting for todo list

--- NotEBookWebNextJS/notebookweb/components/layouts/calendar/Todo_Component.jsx
+++ NotEBookWebNextJS/notebookweb/components/layouts/calendar/Todo_Component.jsx
@@ -11,6 +11,7 @@ export default function Todo_Component({ value, onChangeValue }) {
     const [todo, setTodoList] = useState([]);
     const [date, setDate] = useState(moment());
     const [input, setInput] = useState('');
+    const [selected_todo, setSelectedTodo] = useState('')
 
     const handleChangeEvent = (e) => {
         e.preventDefault();
@@ -31,25 +32,40 @@ export default function Todo_Component({ value, onChangeValue }) {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(date && input)
-            setTodoList([[date, input, ...todo]]);
+        if(date && input){
+            let formattedDate = moment(date).calendar();  
+            let event = input + "--->" + formattedDate;
+            setTodoList([[event,...todo]]);
+        }
+            
 
         //dayStyles(date, value);
+
+    }
+
+    const handleRemove = (e) => {
+        e.preventDefault();
+
+        for(var i = 0; i < todo.length; i++){
+            if(todo[i] == selected_todo){
+                todo.splice(i, 1);
+            }
+        }
 
     }
 
     return (
         <Container>
             <Row className={styles.todo_title}>
-                <Col sm={7} >Agenda</Col>
+                <Col sm={7}>Agenda</Col>
                 <Col sm={2}><Button variant="primary" onClick={handleSubmit}>Add</Button></Col>
-                <Col sm={2}><Button variant="secondary" disabled>Remove</Button></Col>
+                <Col sm={2}><Button variant="secondary" onClick={handleRemove}>Remove</Button></Col>
             </Row>
 
 
             <InputGroup>
                 <InputGroup.Text>Date/Time</InputGroup.Text>
-                <FormControl type="datetime-local" name='Event_Date' placeholder={value.format("MM/DD/YYYY")} onChange={handleChangeDate} />
+                <FormControl type="datetime-local" name='Event_Date' placeholder={value.format('MMMM Do YYYY, h:mm:ss a')} onChange={handleChangeDate} />
             </InputGroup>
 
             <br />
