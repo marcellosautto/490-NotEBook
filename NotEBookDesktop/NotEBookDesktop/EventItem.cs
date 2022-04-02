9de01e1a1e21a1337680c4f4667b9c7c07f207d3
HectorOrtiz618 <90296733+HectorOrtiz618@users.noreply.github.com>
2022-04-02 12:52:59 -0700

    -Added basic TExtEditor for Desktop App
    -Added TExtEditor from Web app to Desktop app
    -Carried Event Formatting From WebApp to to Desktop App
    
    Known Issues:
    -Buttons are missing icons for TExtEditor
    -TExtEditor missing functionality such as hyperlinking, Image insertion, and Strikethrough
    -Buttons are used to change font size, rather than a dropdown menu like WebApp

--- NotEBookDesktop/NotEBookDesktop/EventItem.cs
+++ NotEBookDesktop/NotEBookDesktop/EventItem.cs
@@ -3,7 +3,7 @@
     //This is a class that will contain our events in order to be pushed into EventView in Calender
     class EventItem
     {
-        public string Date { get; set; }
+        public string Date{ get; set; }
         public string Event { get; set; }
     }
 }
