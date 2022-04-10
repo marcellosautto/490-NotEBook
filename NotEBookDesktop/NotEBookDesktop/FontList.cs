48348bcf839b064c3522dd82270b8456eb96f743
HectorOrtiz618 <90296733+HectorOrtiz618@users.noreply.github.com>
2022-04-09 18:08:11 -0700

    Added Editing Icons and DropDown Menu for Fonts
    -Added icons for TExt Editor
    Known Issues:
    -The Increase and Decrease Font icons are bugged because they are large image compressed to a small space
    -Still missing Align Center Icon

---
+++ NotEBookDesktop/NotEBookDesktop/FontList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+
+namespace NotEBookDesktop
+{
+    //Gets the lists of Fonts installed on user's computer
+    class FontList : ObservableCollection<string> 
+    { 
+        public FontList() 
+        {
+            foreach (FontFamily f in Fonts.SystemFontFamilies)
+            {                
+                this.Add(f.ToString());                
+            }  
+        }   
+    }
+}
